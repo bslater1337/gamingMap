91b4dea9f11fef5f2bb4739f892a924d90f1f1f4
Ben Slater <bslater@nmu.edu>
2017-05-08 13:51:55 -0400

    deleted gulpfile

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-//modules used
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var istanbul = require('gulp-istanbul');
-
-gulp.task('travis', ['build', 'testServerJS'], function({
-}));
